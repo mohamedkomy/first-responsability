ee92eb7027c2a228ec3e093afdde664dd624fb1c
mohamedkomy <115396486+mohamedkomy@users.noreply.github.com>
2022-10-09 11:01:39 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,43 @@
+#include "Application/Application.h"
+
+int main(void)
+{
+	APP_init();
+
+	while (1)
+	{
+		APP_start();
+	}
+	
+	
+	
+	
+	
+	
+	
+/*  void DIO_Init();
+	void DIO_SetPin();
+	void DIO_togPin();
+	void DIO_Getpin();
+*/
+
+/*void Button_Init();
+void Get_Button();
+*/
+
+/*
+void Led_Init();
+void LED_ON();
+void LED_OFF();
+void LED_TOGLE();
+
+*/	
+	/*void Timer0_init_N();
+	void Timer0_N_delay_ms();
+*/
+	
+	
+	
+}
+
+
